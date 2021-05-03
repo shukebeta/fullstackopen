c0fcfbc85f5d8db692b27860d6da9ba9aed1e6e6
shukebeta <weizhong2004@gmail.com>
2021-05-03 20:44:34 +1200

    part1: 1.6-1.11 unicafe project done

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react'
+
+const Button = ({text, clickHandler}) => {
+  return (
+    <button onClick={clickHandler}>{text}</button>
+  )
+}
+
+const Row = ({title, value}) => {
+  return (
+    <tr>
+      <td>{title}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Content = ({dataList}) => {
+  console.log(dataList.all)
+  if (dataList.all === 0)
+    return <p>No feedback given</p>
+  return (
+    <div>
+      <table>
+        <tbody>
+        {
+          Object.entries(dataList).map(([key, value], index) => {
+            return <Row title={key} value={value} key={index}/>
+          })
+        }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const getAll = () => good + neutral + bad
+  const getAverage = () => getAll() / 3
+  const getPositive = () => good / getAll() + ' %'
+  const getDataList = () => ({
+    good,
+    neutral,
+    bad,
+    all: getAll(),
+    average: getAverage(),
+    positive: getPositive(),
+  })
+
+  return (
+    <div>
+      <h2> give feedback</h2>
+      <Button text="good" clickHandler={() => setGood(good + 1)}/>
+
+      <Button text="neutral" clickHandler={() => setNeutral(neutral + 1)}/>
+
+      <Button text="bad" clickHandler={() => setBad(bad + 1)}/>
+      <h2>statistics</h2>
+      <Content dataList={getDataList()}/>
+    </div>
+  )
+}
+
+export default App
