8a8f200c681aa87fabeabd046e71f9403c1812c5
shukebeta <weizhong2004@gmail.com>
2021-05-03 19:32:02 +1200

    part1: fix Warning: Each child in a list should have a unique "key" prop

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,7 +12,7 @@ const Content = (props) => {
   return (
     <div>
       {
-        props.parts.map(_ => <Part partName={_.name} exercise={_.exercises} />)
+        props.parts.map((_, index) => <Part partName={_.name} exercise={_.exercises} key={index} />)
       }
     </div>
   )
