a299d8ac1988025bf0654acc019ebd34f987f1b6
shukebeta <weizhong2004@gmail.com>
2021-05-16 00:39:36 +1200

    Part4 Exercises 4.17 done

--- part4/controllers/blog.js
+++ part4/controllers/blog.js
@@ -1,14 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const { title, author, url, likes } = request.body
+  const user = await User.findOne({}).exec()
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+    user: user.id,
+  })
   const savedBlog = await blog.save()
+  user.blogs.push(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
