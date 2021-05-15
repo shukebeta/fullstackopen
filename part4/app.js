3b2e9d19b46b3043e2be8fd9c119b70ae007036f
shukebeta <weizhong2004@gmail.com>
2021-05-15 12:32:27 +1200

    Part4 exercise 4.2: refactoring done

--- part4/app.js
+++ part4/app.js
@@ -1,27 +1,11 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
-
-app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+const cors = require('cors')
+app.use(cors())
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const blogsRouter = require('./controllers/blog')
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
